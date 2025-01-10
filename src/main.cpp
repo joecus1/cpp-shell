4759e5206ecedb81ff4e11b77e406a4af0c32ba1
joecus1 <joecusano57@gmail.comm>
2025-01-09 19:31:28 -0500

    added echo command

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,51 @@
 #include <iostream>
+#include <sstream>
+#include <vector>
 
-int main() {
+int main() 
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  while (true) {
+  while (true) 
+  {
     std::cout << "$ ";
 
     std::string input;
     std::getline(std::cin, input);
 
-    if (input == "exit 0") {
-      return 0;
+    // Split input by spaces
+    std::istringstream iss(input);
+    std::string command;
+    iss >> command;
+
+    std::string arg;
+    std::vector<std::string> args;
+    while (iss >> arg) 
+    {
+      args.push_back(arg);
     }
-    std::cout << input << ": command not found" << std::endl;
+
+    if (command =="echo")
+    {
+      for (const auto& arg : args) 
+      {
+        std::cout << arg << " ";
+      }
+      std::cout << std::endl;
+      continue;
+    }
+    else if (command == "exit") 
+    {
+      if (args.size() > 0 && args[0] == "0") 
+      {
+        return 0;
+      }
+    }
+    else
+    {
+      std::cout << input << ": command not found" << std::endl;
+    }
   }
-  return 0;
 }
