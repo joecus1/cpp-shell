5869700e06aa55bf92d5db83810f4533fe8c7f7b
joecus1 <joecusano57@gmail.comm>
2025-01-09 19:20:29 -0500

    added support for exit command

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,10 @@ int main() {
     std::string input;
     std::getline(std::cin, input);
 
+    if (input == "exit 0") {
+      return 0;
+    }
     std::cout << input << ": command not found" << std::endl;
   }
+  return 0;
 }
