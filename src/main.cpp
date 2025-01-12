f4572c215e170dcda26107d36424d4ba5e5a834f
joecus1 <joecusano57@gmail.comm>
2025-01-12 17:44:32 -0500

    added support for path commands

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,39 @@
 #include <sstream>
 #include <vector>
 #include <algorithm>
+#include <cstdlib>
+#include <filesystem>
+#include <unordered_map>
+
+namespace fs = std::filesystem;
 
 void command_not_found(const std::string& command) 
 {
   std::cout << command << ": not found" << std::endl;
+}
+
+void get_executables_from_dir(
+  const std::string& dir, 
+  std::unordered_map<std::string, std::string>& commands
+)
+{
+  for (const auto& entry : fs::directory_iterator(dir))
+  {
+    // std::cout << entry.path() << std::endl;
+    commands[entry.path().filename()] = entry.path();
+  }
+}
+
+std::unordered_map<std::string, std::string> get_commands_from_path(const std::string& path)
+{
+  std::istringstream iss(path);
+  std::string dir;
+  std::unordered_map<std::string, std::string> commands;
+  while (std::getline(iss, dir, ':'))
+  {
+    get_executables_from_dir(dir, commands);
+  }
+  return commands;
 }
 
 int main() 
@@ -14,6 +43,9 @@ int main()
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
+  const std::string path(std::getenv("PATH"));
+  std::unordered_map<std::string, std::string> path_commands = get_commands_from_path(path);
+  
   while (true) 
   {
     std::cout << "$ ";
@@ -52,11 +84,15 @@ int main()
     }
     else if (command == "type")
     {
-      std::vector<std::string> valid_commands = {"exit", "echo", "type"};
-      if (args.size() > 0 && std::find(valid_commands.begin(), valid_commands.end(), args[0]) != valid_commands.end()) 
+      std::vector<std::string> builtins = {"exit", "echo", "type"};
+      if (args.size() > 0 && std::find(builtins.begin(), builtins.end(), args[0]) != builtins.end()) 
       {
         std::cout << args[0] << " is a shell builtin" << std::endl;
       } 
+      else if(path_commands.find(args[0]) != path_commands.end())
+      {
+        std::cout << args[0] << " is " << path_commands[args[0]] << std::endl;
+      }
       else 
       {
         command_not_found(args[0]);
